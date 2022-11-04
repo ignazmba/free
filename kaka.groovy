f320fd5eaafaffe48aa3817f1a009e1d11aa2440
ignazmba <115480022+ignazmba@users.noreply.github.com>
2022-11-04 12:44:07 -0400

    Update kaka.groovy

--- kaka.groovy
+++ kaka.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Hello') {
             steps {
-                echo 'Hello huhgtuhu'
+                echo 'Hello huhgtrgtuhu'
             }
         }
     }
